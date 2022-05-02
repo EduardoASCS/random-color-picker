96e8ccb9b29f20d5c0604143c78f3d84425d1743
Eduardo Santos <97476026+EduardoASCS@users.noreply.github.com>
2022-05-02 12:28:59 -0300

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export class Button extends React.Component {
-	render() {
-		return (
-			<button 
-				className={ this.props.light ? 'light-button' : 'dark-button' }
-        onClick={this.props.onClick}>
+    render() {
+	return (
+		<button 
+			className={ this.props.light ? 'light-button' : 'dark-button' }
+        		onClick={this.props.onClick}>
 				Refresh
-			</button>
+		</button>
 		);
 	}
 }
